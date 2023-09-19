c28a5d9e013323ce673550378807b5dedc12f364
abdifatah-omar <abdifatahomar2023@gmail.com>
2023-09-19 12:32:48 -0500

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+let currentIndex = 0;
+
+function reset() { for (let i = 0; i < images.length; i++) { images[i].classList.remove('active'); } }
+
+function initializeSlider() { reset(); images[currentIndex].classList.add('active'); }
+
+function slideLeft() { reset(); currentIndex--; if (currentIndex < 0) { currentIndex = images.length - 1; } images[currentIndex].classList.add('active'); }
+
+function slideRight() { reset(); currentIndex++; if (currentIndex >= images.length) { currentIndex = 0; } images[currentIndex].classList.add('active'); }
+
+initializeSlider();
+
+previousImage.addEventListener('click', function() { slideLeft(); });
+
+nextImage.addEventListener('click', function() { slideRight(); });
